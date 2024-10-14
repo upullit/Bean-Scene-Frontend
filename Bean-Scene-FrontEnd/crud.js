b5ea9b7d93c26a5c65f1fe8af48d3f0ad77467e9
Jess Mullins <jessem013@gmail.com>
2024-10-14 11:42:01 +1100

    finished crud functions

---
+++ Bean-Scene-FrontEnd/crud.js
@@ -0,0 +1,77 @@
+//function to create menu items
+const createItem = async (data) => 
+{
+    try {
+        const response = await fetch('/api/menu', 
+            {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+            });
+            return await response.json()
+        }
+        catch (error) 
+            {
+            console.error('Error creaing item', error)
+            }
+ }
+
+// function to get menu items
+const getItems = async () => {
+    try {
+        const response = await fetch('/api/menu');
+        if (!response.ok) throw new Error('Failed to fetch items');
+        return await response.json();     
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+// function to get menu items
+const getSingleItem = async (id) => {
+    try {
+        const response = await fetch(`/api/menu/${id}`);
+        if (!response.ok) throw new Error('Failed to fetch item');
+        return await response.json();     
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+// function to update menu items
+const updateMenuItem = async (id, updatedItem) => {
+    try{
+        const response = await fetch(`/api/menu/${id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(updatedItem)
+    });
+    if (!response.ok){
+        throw new Error('Failed to update the menu item');
+    }
+    return await response.json
+    }
+    catch (error) 
+    {
+    console.error('Error creaing item', error)
+    }
+}
+
+// function to Delete menu items
+const deleteItem = async (id) => {
+    try {
+        const response = await fetch(`/api/menu/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to delete item');
+    }
+
+    console.log('Item deleted');
+    } catch (error) {
+    console.error('Error deleting item', error);
+    }
+}
+
+export {createItem, getItems, getSingleItem, updateMenuItem, deleteItem};
